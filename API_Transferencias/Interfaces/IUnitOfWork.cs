c8f1bd1d327711370772a410ece1c1deab98496f
toffettl <toffetti0824@gmail.com>
2025-05-23 11:18:45 -0300

    feat: UnitOfWork

---
+++ API_Transferencias/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using API_Transferencias.Data;
+
+namespace API_Transferencias.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        Task<int> CommitAsync();
+        Task<int> CompleteAsync();
+        void Rollback();
+    }
+}
---
+++ API_Transferencias/Repositories/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using API_Transferencias.Data;
+
+namespace API_Transferencias.Repositories
+{
+    public class UnitOfWork
+    {
+        private readonly AppDbContext _context;
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> CommitAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
